d1afdc6802e6e59af33b8238da585be5820a1659
ANyssadil <cima.lerouge@gmail.com>
2023-03-16 03:45:43 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
 
 int main(void)
 {
-char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"; 
-fputs(text,stdout);
+char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+fputs(text, stdout);
 return (1);
 }
