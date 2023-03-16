cf5f9cde409da393fe7df0bbe9d98f042f55a046
ANyssadil <cima.lerouge@gmail.com>
2023-03-16 02:19:06 -0700

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -9,7 +9,7 @@
 
 int main(void)
 {
-char text[]= "with proper grammar, but the outcome is a piece of art,";
+char text[] = "with proper grammar, but the outcome is a piece of art,";
 printf("%s\n",text);
 return (0);
 }
