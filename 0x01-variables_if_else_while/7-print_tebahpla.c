2e2039e5cdebe0b1f0ea561f6407451db7e91c4d
ANyssadil <cima.lerouge@gmail.com>
2023-03-17 02:34:30 -0700

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main ()
+{
+        char i;
+
+for (i='z'; i>='a'; i--)
+{
+        putchar(i); 
+        
+}
+
+putchar('\n');
+return (0);
+}
