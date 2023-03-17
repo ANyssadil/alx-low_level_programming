c5325747485e2ff9bede99f577cc7bb7fa17e3e4
ANyssadil <cima.lerouge@gmail.com>
2023-03-17 13:19:11 -0700

    12. Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,39 +14,38 @@ int k;
 int l;
 for (i = 48; i < 58; i++)
 {
-        for (j = 48; j < 58; j++)
-        {
-                for (k = 48; k < 58; k++)
-                {
-                        for (l = 48; l < 58; l++)
-                        {
-                                        if (j<l && i<=k)
-                                        {
-                                                putchar(i);
-                                                putchar(j);
-                                                putchar(32);
-                                                putchar(k);
-                                                putchar(l);
-
-                                                if(i!= 57 || j!=56 || k!=57 || l!=57)
-                                                {
-                                                        /*putchar(',');*/
-                                                        putchar(44);
-                                                        /*putchar(' ');*/
-                                                        putchar(32);
-                                                }
-                                                else
-                                                {
-                                                        break;
-                                                }
-                                        }
-                                        else
-                                        {
-                                                continue;
-                        }               }
-                }
-        }
-
+for (j = 48; j < 58; j++)
+{
+for (k = 48; k < 58; k++)
+{
+for (l = 48; l < 58; l++)
+{
+if (j < l && i <= k)
+{
+putchar(i);
+putchar(j);
+putchar(32);
+putchar(k);
+putchar(l);
+if (i != 57 || j != 56 || k != 57 || l != 57)
+{
+/*putchar(',');*/
+putchar(44);
+/*putchar(' ');*/
+putchar(32);
+}
+else
+{
+break;
+}
+}
+else
+{
+continue;
+}
+}
+}
+}
 }
 
 putchar('\n');
