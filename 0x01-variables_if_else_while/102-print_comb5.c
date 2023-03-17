7ba58e427c1b29bbe47265a7fbb91a015d556782
ANyssadil <cima.lerouge@gmail.com>
2023-03-17 12:42:40 -0700

    12. Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -20,7 +20,7 @@ for (i = 48; i < 58; i++)
                 {
                         for (l = 48; l < 58; l++)
                         {
-                                        if (i!=k || j<l)
+                                        if (j<l && i<=k)
                                         {
                                                 putchar(i);
                                                 putchar(j);
