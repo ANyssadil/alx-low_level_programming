d7c7fc753c298bfa36f1522ed56959c51094e85e
ANyssadil <cima.lerouge@gmail.com>
2023-03-17 04:24:28 -0700

    3. alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
-/* more headers goes there */
+
 
 /**
  * main - Entry point
  *
- * Return: Always 1 (Success)
+ * Return: Always 0 (Success)
  */
 int main ()
 {
-        char i;
+char i;
 
-for (i='a'; i<='z'; i++)
+for (i = 'a'; i <= 'z'; i++)
 {
-        putchar(i);
+putchar(i);
 
 }
 
-for (i='A'; i<='Z'; i++)
+for (i = 'A'; i <= 'Z'; i++)
 {
-        putchar(i);
+putchar(i);
 
 }
 
