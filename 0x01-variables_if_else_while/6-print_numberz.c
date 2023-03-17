e791b4d82a1dceb6bc7f77488a9371378e892990
ANyssadil <cima.lerouge@gmail.com>
2023-03-17 02:29:25 -0700

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main ()
+{
+        int i;
+
+for (i=48; i<58; i++)
+{
+
+  putchar(i); 
+        
+}
+   putchar('\n');
+return (0);
+}
