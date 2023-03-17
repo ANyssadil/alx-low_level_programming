75b1d09420d017d23977ca2d2d6ed574d1c8ae66
ANyssadil <cima.lerouge@gmail.com>
2023-03-17 02:56:50 -0700

    8. Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main ()
+{
+int i;
+   for ( i =48; i <58; i++)
+{
+        
+       
+        putchar(i);
+
+}
+for ( i ='a'; i <'g'; i++)
+{
+        
+       
+        putchar(i);
+
+}
+
+putchar('\n');
+return (0);
+}
