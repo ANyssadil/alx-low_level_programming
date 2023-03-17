1f44f225b0830a871611ffb2131127d61b25e61f
ANyssadil <cima.lerouge@gmail.com>
2023-03-17 09:07:42 -0700

    12. Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+int j;
+int k;
+int l;
+for (i = 48; i < 58; i++)
+{
+        for (j = 48; j < 58; j++)
+        {
+                for (k = 48; k < 58; k++)
+                {
+                        for (l = 48; l < 58; l++)
+                        {
+                                        if (j<l)
+                                        {
+                                                putchar(i);
+                                                putchar(j);
+                                                putchar(32);
+                                                putchar(k);
+                                                putchar(l);
+
+                                                if(i!= 57 || j!=56 || k!=57 || l!=57)
+                                                {
+                                                        /*putchar(',');*/
+                                                        putchar(44);
+                                                        /*putchar(' ');*/
+                                                        putchar(32);
+                                                }
+                                                else
+                                                {
+                                                        break;
+                                                }
+                                        }
+                                        else
+                                        {
+                                                continue;
+                        }               }
+                }
+        }
+
+}
+
+putchar('\n');
+
+return (0);
+}
