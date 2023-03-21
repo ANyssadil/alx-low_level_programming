3bd072ecdbb39176254f575b6a9146417d9d636f
ANyssadil <cima.lerouge@gmail.com>
2023-03-21 07:30:00 -0700

    8. Im federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,7 +2,6 @@
 #include "main.h"
 /**
  * jack_bauer- prints every minute of the day of Jack Bauer.
- * 
  * Return: void
  */
 void jack_bauer(void)
@@ -11,43 +10,35 @@ int i;
 int j;
 int k;
 int l;
-for ( i = 0; i <= 2; i++)
+for (i = 0; i <= 2; i++)
 {
-if (i!=2)
+if (i != 2)
 {
-for ( j = 0; j <= 9; j++)
+for (j = 0; j <= 9; j++)
 {
-for ( k = 0; k <=5; k++)
+for (k = 0; k <= 5; k++)
 {
-for ( l = 0; l <=9; l++)
+for (l = 0; l <= 9; l++)
 {
-_putchar(i+'0');
-_putchar(j+'0');
+_putchar(i + '0');
+_putchar(j + '0');
 _putchar(':');
-_putchar(k+'0');
-_putchar(l+'0');
+_putchar(k + '0');
+_putchar(l + '0');
 _putchar('\n');
-}
-}
-}
-}
+}}}}
 else
 {
-for ( j = 0; j <= 3; j++)
+for (j = 0; j <= 3; j++)
 {
-for ( k = 0; k <=5; k++)
+for (k = 0; k <= 5; k++)
 {
-for ( l = 0; l <=9; l++)
+for (l = 0; l <= 9; l++)
 {
-_putchar(i+'0');
-_putchar(j+'0');
+_putchar(i + '0');
+_putchar(j + '0');
 _putchar(':');
-_putchar(k+'0');
-_putchar(l+'0');
+_putchar(k + '0');
+_putchar(l + '0');
 _putchar('\n');
-}
-}
-}
-}
-}
-}
+}}}}}}
