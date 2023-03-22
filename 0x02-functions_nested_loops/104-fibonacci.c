ee25c971fb48c8679a1d0242be1904d94723b073
ANyssadil <cima.lerouge@gmail.com>
2023-03-22 07:57:51 -0700

    16. In computer class, the first assignment was to write a program to print the first 100 Fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,23 +5,13 @@
  */
 int main(void)
 {
-unsigned long int a = 1;
-unsigned long int b = 1;
+unsigned long int a = 1, b = 1;
 int i;
-unsigned long int c;
-unsigned long int d=1000000000;
-unsigned long int r;
+unsigned long int c, r;
+unsigned long int d = 1000000000;
+unsigned long int a_first, a_sec, b_first, b_sec, c_first, c_sec;
 
-unsigned long int a_first;
-unsigned long int a_sec;
-unsigned long int b_first;
-unsigned long int b_sec;
-unsigned long int c_first;
-unsigned long int c_sec;
-
-
-printf("%lu", b);
-
+		printf("%lu", b);
 
 		for (i = 0; i < 90; i++)
 		{
@@ -31,31 +21,25 @@ printf("%lu", b);
 		b = c;
 		}
 
-		a_first=a/d;
-		a_sec=a%d;
-		b_first=b/d;
-		b_sec=b%d;
+		a_first = a / d;
+		a_sec = a % d;
+		b_first = b / d;
+		b_sec = b % d;
 
-   
-
-		for ( i = 90; i < 97; i++)
+		for (i = 90; i < 97; i++)
 		{
-			r=(a_sec+b_sec)/d;
-			c_first=a_first+b_first+r;
-			c_sec=(a_sec+b_sec)-d*r; 
+			r = (a_sec + b_sec) / d;
+			c_first = a_first + b_first + r;
+			c_sec = (a_sec + b_sec) - d * r;
 
 			printf(", %lu", c_first);
 			printf("%lu", c_sec);
 
-			a_first=b_first;
-			a_sec=b_sec;
-			b_first=c_first;
-			b_sec=c_sec;
-
+			a_first = b_first;
+			a_sec = b_sec;
+			b_first = c_first;
+			b_sec = c_sec;
 		}
 printf("\n");
 return (0);
-} 
-
-
-
+}
