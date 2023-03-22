50d5108cdfb201c1acf429090ff18f27d4fa7eb2
ANyssadil <cima.lerouge@gmail.com>
2023-03-22 07:45:55 -0700

    16. In computer class, the first assignment was to write a program to print the first 100 Fibonacci numbers. I

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,23 +9,53 @@ unsigned long int a = 1;
 unsigned long int b = 1;
 int i;
 unsigned long int c;
+unsigned long int d=1000000000;
+unsigned long int r;
 
-printf("%lu, ", b);
-for (i = 0; i < 98; i++)
-{
-c = a + b;
+unsigned long int a_first;
+unsigned long int a_sec;
+unsigned long int b_first;
+unsigned long int b_sec;
+unsigned long int c_first;
+unsigned long int c_sec;
 
-if (i != 97)
-{
-printf("%lu, ", c);
-}
-else
-{
-printf("%lu", c);
-}
-a = b;
-b = c;
-}
+
+printf("%lu", b);
+
+
+		for (i = 0; i < 90; i++)
+		{
+		c = a + b;
+		printf(", %lu", c);
+		a = b;
+		b = c;
+		}
+
+		a_first=a/d;
+		a_sec=a%d;
+		b_first=b/d;
+		b_sec=b%d;
+
+   
+
+		for ( i = 90; i < 97; i++)
+		{
+			r=(a_sec+b_sec)/d;
+			c_first=a_first+b_first+r;
+			c_sec=(a_sec+b_sec)-d*r; 
+
+			printf(", %lu", c_first);
+			printf("%lu", c_sec);
+
+			a_first=b_first;
+			a_sec=b_sec;
+			b_first=c_first;
+			b_sec=c_sec;
+
+		}
 printf("\n");
 return (0);
-}
+} 
+
+
+
