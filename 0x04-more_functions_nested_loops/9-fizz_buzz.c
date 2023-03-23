42238a6f69ddc36e362fab1a5e85c6bd88138abb
ANyssadil <cima.lerouge@gmail.com>
2023-03-23 03:14:18 -0700

    9. Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * main- prints the numbers from 1 to 100. Fizz.Buzz style.
+ * Return: void
+ */
+int main(void)
+{
+	int i;
+
+for (i = 1; i < 100; i++)
+{
+	if (i % 3 == 0)
+	{
+		printf("Fizz ");
+	}
+	else if (i % 5 == 0)
+	{
+		printf("Buzz ");
+	}
+	else if (i % 3 == 0 && i % 5 == 0)
+	{
+		printf("FizzBuzz ");
+	}
+	else
+	{
+		printf("%d ", i);
+	}
+
+
+}
+printf("Buzz\n");
+
+	return (0);
+
+}
