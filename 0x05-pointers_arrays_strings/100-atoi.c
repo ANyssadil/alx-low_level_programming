f118ae207f2cde76f710ef2a4bd4dd47a70a6a52
ANyssadil <cima.lerouge@gmail.com>
2023-03-27 14:39:04 -0700

    10. Great leaders are willing to sacrifice the numbers to save the people. Poor leaders sacrifice the people to save the numbers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -5,48 +5,43 @@
  * @s: string to convert.
  * Return: integer.
  */
-#include <stdio.h>
 int _atoi(char *s)
 {
-int i=0, j=0;
-int sign=1; 
-int flag=1; 
-unsigned int n=0; 
-int minus=0, plus=0;
+int i = 0, j = 0, sign = 1, flag = 1, minus = 0, plus = 0;
+unsigned int n = 0;
 
-	while (*(s + i) != '\0')
+while (*(s + i) != '\0')
+{
+	if (*(s + i) >= '0' && *(s + i) <= '9')
 	{
-		if (*(s + i) >= '0' && *(s + i) <= '9') 
+		n = n * 10 + (*(s + i) - '0');
+
+
+		for (j = 0; j <= i; j++)
 		{
-		 n = n * 10 + (*(s + i) - '0');
-  
-			
-			for (j=0; j<=i;j++)
+			if (*(s + j) == '-')
 			{
-				if (*(s+j)== '-')
-				{
-					minus++;
-				}
-				else if (*(s+j)== '+')
-				{
-					plus++;
-				}
+				minus++;
 			}
-		 if ((minus > plus && i>=1) || (minus %2 != 0 && i>=1) )
-		 {
-			sign = -1;
-		 }
-		
-		flag = 0; 
-	   }
-	   
-	   else if (flag == 0 && !(*(s + i) >= '0' && *(s + i) <= '9'))
-	   {
-		 break; 
+			else if (*(s + j) == '+')
+			{
+				plus++;
+			}
 		}
-	   i++; 
-	 
-	}	
-	
-	return (sign * n);
+		if ((minus > plus && i >= 1) || (minus % 2 != 0 && i >= 1))
+		{
+			sign = -1;
+		}
+
+		flag = 0;
+	}
+
+	else if (flag == 0 && !(*(s + i) >= '0' && *(s + i) <= '9'))
+	{
+		break;
+	}
+	i++;
 }
+return (sign * n);
+}
+
