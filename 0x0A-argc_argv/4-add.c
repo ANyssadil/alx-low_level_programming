5732fe6665cd30273bd8f35c8309702e1358cd87
ANyssadil <cima.lerouge@gmail.com>
2023-04-08 21:49:10 -0700

    4. To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,9 +13,9 @@ int main(int argc, char *argv[])
 	int sum = 0;
 	int flag = 1;
 
-	if (argc <= 2)
+	if (argc < 2)
 		printf("%d\n", sum);
-	else if (argc > 2)
+	else if (argc >= 2)
 	{
 	for (i = 1; i < argc; i++)
 	{
