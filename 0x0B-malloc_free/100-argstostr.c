497fb124298d2887208cce08540b79aa8e58044a
ANyssadil <cima.lerouge@gmail.com>
2023-04-12 00:53:00 -0700

    5. It isnt the mountains ahead to climb that wear you out; its the pebble in your shoe

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -3,40 +3,40 @@
 #include <stdlib.h>
 
 /**
- * argstostr - concatenates all the arguments of a program.
- *@ac:
- *@av:
- * Return: .
+ * argstostr - deconcatenates all the arguments of a program.
+ *@ac: number of arguments in the command line.
+ *@av: 2D array where arguments are stored.
+ * Return: pointer to a new string, or NULL if it fails.
  */
 char *argstostr(int ac, char **av)
 {
 	char *ptr;
-	int i, j, k, c=0;
+	int i, j, k = 0, c = 0;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
-	for (i=0; i<ac; i++)
+	for (i = 0; i < ac; i++)
 	{
-		j=0;
-		while (av[i][j]!='\0')
-		{	
+		j = 0;
+		while (av[i][j] != '\0')
+		{
 			j++;
 			c++;
 		}
 		c++;
 	}
 
-	ptr=(char*)malloc((1 + c) * sizeof(char));
+	ptr = (char *)malloc((1 + c) * sizeof(char));
 	if (ptr == NULL)
 		return (NULL);
-	
-	for (i=0; i<ac; i++)
+
+	for (i = 0; i < ac; i++)
 	{
-		for (j=0, k=0; av[i][j]!= '\0'; j++,k++)
-			ptr[k]=av[i][j];
-		ptr[k]='\0';
+		for (j = 0; av[i][j] != '\0'; j++, k++)
+			ptr[k] = av[i][j];
+		ptr[k] = '\n';
 		k++;
 	}
-		ptr[k]='\0';
+		ptr[k] = '\0';
 		return (ptr);
 }
