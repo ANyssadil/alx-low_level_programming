f59b85cf1027f1b046a2e5c81d6e9b88114038a5
ANyssadil <cima.lerouge@gmail.com>
2023-04-16 23:58:53 -0700

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - check  new type struct dog
+ * @name: dog name.
+ * @age: dog age.
+ * @owner: dog owner.
+ * Return: Always 0.
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+#endif
